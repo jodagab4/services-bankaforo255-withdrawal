53ed2e5d89dd408759cd2c0e49fc37b9d5d644db
Jorge Galeas <jodagab4@hotmail.com>
2020-04-07 22:04:42 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# services-bankaforo255-withdrawal
+by Jorge Galeas jodagab4@hotmail.com
---
+++ src/main/java/com/app/aforo255/withdrawal/service/ITransactionService.java
@@ -0,0 +1,9 @@
+package com.app.aforo255.withdrawal.service;
+
+import com.app.aforo255.withdrawal.domain.Transaction;
+
+public interface ITransactionService {
+
+	public Transaction save (Transaction transaction );
+}
+
